7fbc03b4f725636e6f6bd3e41cf75105e6103a3b
jiwon <angry9908@gmail.com>
2024-11-06 14:35:47 +0900

    setting: bottomNavigationBar 추가

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatefulWidget {
+  const Homescreen({super.key});
+
+  @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+
+  int _menuIndex=0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _menuIndex,
+        // 여기 배열에 페이지 생성
+        children: [
+
+        ],
+      ),
+
+      bottomNavigationBar: NavigationBar(
+          selectedIndex: _menuIndex,
+          destinations: [
+            NavigationDestination(
+                icon: Icon(Icons.home_outlined,),
+                label: "홈"
+            ),
+            NavigationDestination(
+                icon: Icon(Icons.article_outlined),
+                label: "게시판"
+            ),
+            NavigationDestination(
+                icon: Icon(Icons.map_outlined),
+                label: "지도"
+            ),
+            NavigationDestination(
+                icon: Icon(Icons.star_outline_rounded),
+                label: "북마크"
+            ),
+            NavigationDestination(
+                icon: Icon(Icons.person_outline),
+                label: "마이페이지"
+            ),
+          ]
+      ),
+    );
+  }
+}
